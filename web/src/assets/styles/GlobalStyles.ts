89e332cb619ccca22b7c23ca438e6224a3954fdb
Leonardo Santos <leonardo.santos@codificar.com.br>
2020-10-24 14:45:15 -0300

    update global styles

--- web/src/assets/styles/GlobalStyles.ts
+++ web/src/assets/styles/GlobalStyles.ts
@@ -4,17 +4,6 @@ export const GlobalStyles = createGlobalStyle`
 	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
 	@import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
 
-	* {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
-	}
-
-	button, input, textarea {
-		outline: 0;
-		border: 0;
-	}
-
 	:root {
 		--primary: #0288D1;
 		--background-primary: #282C34;
@@ -22,5 +11,25 @@ export const GlobalStyles = createGlobalStyle`
 		--text-regular: #FEFEFE;
 		--text-muted: #767676;
 		--danger: #E17A6C;
+	}
+
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+
+		color: var(--text-regular);
+		font-family: 'Open Sans';
+	}
+
+	html, body, #root {
+		width: 100vw;
+		height: 100vh;
+		background-color: var(--background-primary);
+	}
+
+	button, input, textarea {
+		outline: 0;
+		border: 0;
 	}
 `;
