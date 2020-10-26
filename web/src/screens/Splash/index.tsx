cec73b69e0117ad3dd8f82b58b1521236b5a959f
Leonardo Santos <leonardo.santos@codificar.com.br>
2020-10-26 00:30:16 -0300

    add backup download button

--- web/src/screens/Splash/index.tsx
+++ web/src/screens/Splash/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
-import { Container, SplashImage, Title, SubTitle, CodeBox, Link } from './styles';
+import {
+	Container,
+	SplashImage,
+	Title,
+	SubTitle,
+	CodeBox,
+	Link,
+} from './styles';
 
 const Splash: React.FC = () => {
+	function handleClickBackup() {
+		const json = localStorage.getItem('pages');
+		if (!json) return;
+
+		const pages = JSON.parse(json);
+		const data = new Blob([JSON.stringify(pages, null, 2)], { type: json });
+		const a = document.createElement('a');
+		a.href = URL.createObjectURL(data);
+		a.download = 'pages.json';
+		a.click();
+		a.remove();
+	}
+
 	return (
 		<Container>
 			<SplashImage />
@@ -13,6 +33,7 @@ const Splash: React.FC = () => {
 				... ou utilize o atalho <CodeBox>Ctrl + N</CodeBox>
 			</SubTitle>
 			<Link>Ver lista de atalhos</Link>
+			<Link onClick={handleClickBackup}>Fazer backup</Link>
 		</Container>
 	);
 };
