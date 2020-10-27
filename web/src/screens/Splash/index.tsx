13aa46db75c6a4e79cda2d73c5ce5becdfafdf06
Leonardo Santos <leonardo.santos@codificar.com.br>
2020-10-27 20:58:16 -0300

    finish backup functions

--- web/src/screens/Splash/index.tsx
+++ web/src/screens/Splash/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import downloadData from '../../utils/downloadData';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { Page, usePage } from '../../store/pages';
+import downloadData, { uploadData } from '../../utils/backup';
 
 import {
 	Container,
@@ -11,6 +12,10 @@ import {
 } from './styles';
 
 const Splash: React.FC = () => {
+	const [uploadContent, setUploadContent] = useState<Page[] | null>(null);
+
+	const {setPages} = usePage();
+
 	function handleClickBackup() {
 		const json = localStorage.getItem('pages');
 		if (!json) return;
@@ -18,6 +23,16 @@ const Splash: React.FC = () => {
 		const pages = JSON.parse(json);
 		downloadData(pages);
 	}
+
+	function handleUpload(e: ChangeEvent<HTMLInputElement>) {
+		if (!e.currentTarget.files) return
+		uploadData(e.currentTarget.files[0], setUploadContent);
+	}
+
+	useEffect(() => {
+		if (!uploadContent) return;
+		setPages(uploadContent);
+	}, [uploadContent])
 
 	return (
 		<Container>
@@ -30,6 +45,7 @@ const Splash: React.FC = () => {
 			</SubTitle>
 			<Link>Ver lista de atalhos</Link>
 			<Link onClick={handleClickBackup}>Fazer backup</Link>
+			<input type="file" name="backup" id="backup" onChange={handleUpload} />
 		</Container>
 	);
 };
---
+++ web/src/utils/backup.ts
@@ -0,0 +1,22 @@
+import { Page } from "../store/pages";
+
+export default function downloadData(data: any) {
+	const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'json' });
+	const a = document.createElement('a');
+	a.href = URL.createObjectURL(blob);
+	a.download = 'pages.json';
+	a.click();
+	a.remove();
+}
+
+export function uploadData(file: File, setState: React.Dispatch<Page[]>) {
+	const reader = new FileReader();
+	reader.readAsText(file);
+
+	return reader.onload = (event): Object | undefined => {
+		const result = event.target?.result;
+		if (!result) return;
+
+		setState(JSON.parse(result.toString()));
+	}
+}
